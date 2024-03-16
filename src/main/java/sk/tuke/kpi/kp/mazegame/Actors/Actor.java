056889ed27cdf5fa1cec285ef9c91c0bf7033148
Oleksandr Kryvolapov <oleksandr.kryvolapov@student.tuke.sk>
2024-03-16 22:03:34 +0100

    Update 38 files
    - /src/main/resources/maps/bigone.txt
    - /src/main/resources/maps/maze.txt
    - /src/main/resources/maps/sample.txt
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actions/Move.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/AbstractActor.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Actor.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Exit.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Key.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Player.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Sprite.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Actors/SpriteColors.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Gamefield/Map.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Gamefield/MapBuilder.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Gamefield/Tile.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Gamefield/TileType.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/ConsoleUI.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Game.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Gamestate.java
    - /src/main/java/sk/tuke/kpi/kp/mazegame/Main.java
    - /src/main/gamestudio-7784.main.iml
    - /out/production/gamestudio-7784/.gitignore
    - /out/production/gamestudio-7784/.name
    - /out/production/gamestudio-7784/discord.xml
    - /out/production/gamestudio-7784/gamestudio-7784.iml
    - /out/production/gamestudio-7784/misc.xml
    - /out/production/gamestudio-7784/modules.xml
    - /out/production/gamestudio-7784/uiDesigner.xml
    - /out/production/gamestudio-7784/vcs.xml
    - /out/production/gamestudio-7784.main/maps/sample.txt
    - /out/production/gamestudio-7784.main/main/resources/maps/sample.txt
    - /out/production/gamestudio-7784.main/main/gamestudio-7784.main.iml
    - /.idea/.gitignore
    - /.idea/.name
    - /.idea/gamestudio-7784.iml
    - /.idea/misc.xml
    - /.idea/modules.xml
    - /.idea/uiDesigner.xml
    - /.idea/vcs.xml

--- src/main/java/sk/tuke/kpi/kp/mazegame/Actors/Actor.java
+++
@@ -1,7 +0,0 @@
-package sk.tuke.kpi.kp.mazegame.Actors;
-public interface Actor {
-    int getPosX();
-    int getPosY();
-    void setPosition(int posX, int posY);
-
-}
--- src/main/java/sk/tuke/kpi/kp/mazegame/Gamestate.java
+++
@@ -1,5 +0,0 @@
-package sk.tuke.kpi.kp.mazegame;
-
-public enum Gamestate {
-    PLAYING,WON,FAILED;
-}
